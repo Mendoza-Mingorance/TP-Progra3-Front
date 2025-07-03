6d6ee268a10e2ec370166c25a884201ed476dce2
BrayanMen <brayanjmr880@gmail.com>
2025-07-03 11:06:44 -0300

    Add Navbar, logo,theme and css basic

---
+++ js/theme.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const changeThemeCheck = document.getElementById('theme_dark');
+    const changeThemeIcon = document.getElementById('theme_icon');
+
+    const storageTheme = localStorage.getItem('tema') || 'light';
+    applyTheme(storageTheme);
+
+    changeThemeCheck.checked = storageTheme === 'dark';
+    putIcon(storageTheme);
+
+    changeThemeCheck.addEventListener('change', () => {
+        const newTheme = changeThemeCheck.checked ? 'dark' : 'light';
+        applyTheme(newTheme);
+    });
+
+    function applyTheme(tema) {
+        document.documentElement.setAttribute('data-theme', tema);
+        localStorage.setItem('tema', tema);
+        putIcon(tema);
+    }
+
+    function putIcon(tema) {
+        changeThemeIcon.textContent = tema === 'dark' ? '☀️' : '🌙';
+    }
+});
